8792a3f7cd2acdba70648d18cc60ea550fdfae12
Valritehachi <valritehachi@gmail.com>
2024-03-06 00:23:19 -0800

    edits

---
+++ console.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""Defines the HBnB console."""
+import cmd
+import re
+from shlex import split
+"""from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+"""
+
+def parse(arg):
+    """Parse the command argument."""
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the HolbertonBnB command interpreter.
+
+    Attributes:
+        prompt (str): The command prompt.
+    """
+
+    prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
+
+    def emptyline(self):
+        """Do nothing upon receiving an empty line."""
+        pass
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print("")
+        return True
+
+    def help_quit(self):
+        """Quit command to exit the program"""
+        print("Quit command to exit the program")
+
+    def do_help(self, arg):
+        """Get help on commands"""
+        cmd.Cmd.do_help(self, arg)
+
+    def do_create(self, arg):
+        """Create a new class instance and print its id."""
+        argl = parse(arg)
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            print(eval(argl[0])().id)
+            storage.save()
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
